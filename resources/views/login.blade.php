13796dc74d561fc0b3721fe14e9bf5f6968ed8b2
aucucho2 <97245247+aucucho2@users.noreply.github.com>
2022-01-06 21:39:23 +0200

    arlindo

---
+++ resources/views/login.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My Web Page</title>
+    </head> 
+    <body>
+        <div class="container">
+            <div class-center>
+        <h1 class="text-center title">SITE EM CONSTRUCAO</h1>
+        <div class="texe-center subtitle>">
+            Estamos trabalhando para lhe oferecer o melhor</div>
+        </div>
+        <img src="{{ asset('images/Arlindo.jpg') }}" style="width:350px; height: 350px">
+        <br>
+        <br>
+        <input type="text" name="caraguejo">
+         <br> 
+         <br>
+        <input type="text" name="caraguejo">
+    </body>      
+</html>    
